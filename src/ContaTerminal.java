94a00459ea32625a95410e691c237e69551214d8
glauco <glauquinho2@gmail.com>
2024-04-29 18:19:43 -0300

    commit final do projeto conta banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,31 @@
+//import da biblioteca Scanner para utilização nos inputs
+//import da biblioteca Locale para utilização nos valores númericos
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        //Criando o Scanner e definindo o Locale
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    
+        //Solicitação das informações do Usuário para criação de conta
+        System.out.print("Digite o número da agência: ");
+        String numeroAgencia = scanner.next();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Digite o valor que deseja inserir: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("");
+
+        //Exibição das mensagem de sucesso ao criar conta
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldo + "R$ já está disponível para saque!");
+
     }
 }
